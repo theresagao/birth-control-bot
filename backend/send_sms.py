4f8faea7676f1e73777308dac24995daac9a6447
Sarah Sukardi <sukardi@jhu.edu>
2016-07-23 14:49:26 -0700

    Setup with Heroku

--- backend/send_sms.py
+++ backend/send_sms.py
@@ -1,11 +1,11 @@
 from twilio.rest import TwilioRestClient
 
-account_sid = "{{my-id}}" # Your Account SID from www.twilio.com/console
-auth_token  = "{{my-token}}"  # Your Auth Token from www.twilio.com/console
+account_sid = "ACd1a5bc0ab62b14c0f0429e5f2c224a65" # Your Account SID from www.twilio.com/console    
+auth_token  = "a77d83bdf3b7b7b7277568c1b7ffd32a"  # Your Auth Token from www.twilio.com/console
 
 client = TwilioRestClient(account_sid, auth_token)
 
-message = client.messages.create(body="Hello from Python",
+message = client.messages.create(body="What would you like?",
     to="+19492935943",    # Replace with your phone number
     #from_="+15005550006")
     from_="+19493972430") # Replace with your Twilio number
